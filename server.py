979cbe87ae56522be18c2036eb7cdba7608f5f0e
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-08-16 18:20:09 -0400

    server.py:  Now works as a middleman between psql and 3dCart
    This works with one very specific configuration.
    'select * from orders where InvoiceNumber=<invoice_number>;'
    
    Testing using UPS Worldship and the Postgres ODBC driver is needed.

--- server.py
+++ server.py
@@ -1,12 +1,53 @@
 #!/usr/bin/python
 
 import SocketServer
-from modules.psql_server import psql_server
+import modules.psql_server as psql_server
+from modules.rest_client import Rest_3Dcart
 import logging
+from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 
-server = SocketServer.TCPServer(("0.0.0.0", 5432), psql_server)
+class psql_3dCart(psql_server.psql_server):
+    api = Rest_3Dcart('','','')
+
+    def handle_query(self,query):
+        """
+        Handle SQL Querys
+        Specifically designed to give order informfation to this query:
+        'select * from orders where InvoiceNumber=<invoice_number>;'
+        """
+        invoice_number = None
+        response = None
+
+        query = query.lower().strip(';')
+        words = query.split(' ')
+
+        query_type    = words[0]
+        if 'from' in words:
+            from_position = words.index('from')
+        else:
+            from_position = -1
+        if 'where' in words:
+            where_position = words.index('where')
+        else:
+            where_position = -1
+
+        if(query_type == "select" and where_position != -1):
+            where_clauses = words[where_position+1:]
+            for clause in where_clauses:
+                where_pair = clause.split('=')
+                if('invoicenumber' == where_pair[0]):
+                    invoice_number = where_pair[1]
+            if(invoice_number != None):
+                logging.info("Getting order information for invoice number: {}".format(invoice_number))
+                response = self.api.get_order_by_invoice_number(invoice_number)
+                self.select_response(response)
+        #Default if command does not fit specified syntax
+        self.send_data(psql_server.CommandComplete(query_type+" 0"))
+        self.send_data(psql_server.ReadyForQuery)
+
+server = SocketServer.TCPServer(("0.0.0.0", 5432), psql_3dCart)
 try:
     server.serve_forever()
 except:
