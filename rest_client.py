f2a044479a0250a8856c63076703c524daa3f014
Arthur Moore <Arthur.Moore.git@cd-net.net>
2017-08-16 12:07:19 -0400

    rest_client.py:  WIP:  Make the code more modular
    add a rest_get function for generic get requests

--- rest_client.py
+++ rest_client.py
@@ -3,11 +3,20 @@ import urllib2
 import json
 from pprint import pprint
 
-
 PRIVATE_KEY = ''
+
+def rest_get(url,parameters,headers):
+    query = urllib.urlencode(parameters)
+    full_url = url + '?' + query
+
+    req = urllib2.Request(full_url, None, headers)
+    response = urllib2.urlopen(req)
+    data = response.read()
+    return json.loads(data)
 
 def get_order_by_invoice_number(invoice_number,site_url,site_token):
     rest_base = 'https://apirest.3dcart.com/3dCartWebAPI/v1/'
+    url = rest_base + 'Orders'
     headers = {
         'Content-Type': 'application/json;charset=UTF-8',
         'Accept': 'application/json',
@@ -15,15 +24,6 @@ def get_order_by_invoice_number(invoice_number,site_url,site_token):
         'PrivateKey': PRIVATE_KEY,
         'Token':     site_token
         }
-
     parameters = {'invoicenumber': invoice_number}
-    query = urllib.urlencode(parameters)
+    return rest_get(url,parameters,headers)
 
-    url = rest_base + 'Orders?'+query
-
-    req = urllib2.Request(url, None, headers)
-
-    response = urllib2.urlopen(req)
-
-    data = response.read()
-    return json.loads(data)
